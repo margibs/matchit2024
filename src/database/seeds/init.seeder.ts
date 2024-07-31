7d386746b92a7e6ddef917bde57189950798432c
Rumar Gibs Ecaldre <margibs_hbb@yahoo.com>
2024-07-31 21:02:27 +0800

    Turning off seeders

--- src/database/seeds/init.seeder.ts
+++ src/database/seeds/init.seeder.ts
@@ -8,7 +8,8 @@ import WinningCombinationSeeder from './winning-combination.seeder';
 export default class InitSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<any> {
     await runSeeders(dataSource, {
-      seeds: [BoardSeeder, TimezoneSeeder, WinningCombinationSeeder],
+      // seeds: [BoardSeeder, TimezoneSeeder, WinningCombinationSeeder],
+      seeds: [],
     });
   }
 }
