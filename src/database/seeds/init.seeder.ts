9614bb208c4a8bcf96fce7a3255058c3c60bffb3
Rumar Gibs Ecaldre <margibs_hbb@yahoo.com>
2024-08-02 22:10:58 +0800

    Comment unused import

--- src/database/seeds/init.seeder.ts
+++ src/database/seeds/init.seeder.ts
@@ -1,8 +1,8 @@
 import { DataSource } from 'typeorm';
 import { runSeeders, Seeder } from 'typeorm-extension';
-import TimezoneSeeder from './timezone.seeder';
-import BoardSeeder from './board.seeder';
-import WinningCombinationSeeder from './winning-combination.seeder';
+// import TimezoneSeeder from './timezone.seeder';
+// import BoardSeeder from './board.seeder';
+// import WinningCombinationSeeder from './winning-combination.seeder';
 
 // TODO: turn off seeder when done to prevent data loss
 export default class InitSeeder implements Seeder {
